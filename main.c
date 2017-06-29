991c247fa3b753d66de5f56919da76a52dbc59d3
Matt Martineau <mm14628@bristol.ac.uk>
2017-06-29 11:22:09 -0700

    Removed reference to pad

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ int main(int argc, char** argv)
   mesh.global_nx = get_int_parameter("nx", flow_params);
   mesh.global_ny = get_int_parameter("ny", flow_params);
   mesh.niters = get_int_parameter("iterations", flow_params);
-  mesh.pad = PAD;
+  mesh.pad = 2;
   mesh.local_nx = mesh.global_nx+2*mesh.pad;
   mesh.local_ny = mesh.global_ny+2*mesh.pad;
   mesh.width = get_double_parameter("width", ARCH_ROOT_PARAMS);
